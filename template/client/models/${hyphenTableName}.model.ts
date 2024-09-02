d83d8004f3e0d6d2cd6ee400ad7ce2b1f31c3348
HUYUANDONG <952086299@qq.com>
2024-09-02 13:44:18 +0800

    feature: 修改基础模板

--- template/client/models/${hyphenTableName}.model.ts
+++ template/client/models/${hyphenTableName}.model.ts
@@ -1,4 +1,4 @@
-import {BaseModel} from "./index";
+import {BaseModel} from "./base.model.ts";
 
 export interface <%=table.upperCamelName%>Model extends BaseModel{
     <%table.fieldArray.filter(function(field){return !field.ignore}).forEach(function(field){%>
